86d1c95d6b89d4cb2b7a3445e9572c3b51e8a5cc
natebag <njatobinyt@gmail.com>
2026-03-21 12:35:36 -0400

    feat: decompile 63 functions from loading/timer/CTG/stringbuffer systems
    - LoadingScreen, MoviePlayer, OptionsRecon
    - Timer (GetTime, GetTicks), SRand
    - StringBuffer (ctor, erase, capacity, length, c_str)
    - CTGFile NGC implementation (read/write/seek/tell)
    - CoordConversions (AltToWorld, WorldToAlt)

---
+++ include/core/movieplayer.h
@@ -0,0 +1,39 @@
+#ifndef MOVIEPLAYER_H
+#define MOVIEPLAYER_H
+
+#include "types.h"
+
+// Forward declarations
+class ERC;
+class TheSimsStateMachine;
+
+// MoviePlayerEndgameMovieState - State for playing endgame FMV
+// From: movieplayer.obj
+// Size: at least 0x1C bytes
+// Layout:
+//   0x00: int m_movieId
+//   0x04: int m_field04 (init to -1)
+//   0x08: int m_field08 (init to 0, holds state machine ptr)
+//   0x0C: int m_field0C (init to -1)
+//   0x10: int m_field10 (init to 0)
+//   0x14: int m_field14 (init to 1)
+//   0x18: void* m_vtable (set to vtable ptr)
+class MoviePlayerEndgameMovieState {
+public:
+    MoviePlayerEndgameMovieState(int movieId);
+
+    void Draw(ERC* rc);
+    void Shutdown(void);
+    void SetFlowStateCurrent(void);
+
+private:
+    int m_movieId;       // 0x00
+    int m_field04;       // 0x04
+    int m_field08;       // 0x08 - TheSimsStateMachine*
+    int m_field0C;       // 0x0C
+    int m_field10;       // 0x10
+    int m_field14;       // 0x14
+    void* m_vtable18;    // 0x18
+};
+
+#endif // MOVIEPLAYER_H
