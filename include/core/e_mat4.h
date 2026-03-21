5389d96a860f3426422304b0d623d7c2ff2aecb7
natebag <njatobinyt@gmail.com>
2026-03-21 15:29:28 -0400

    feat: decompile 200 small functions from lighting, level, matrix, and model systems
    Decompile small functions (<=64 bytes) from engine/base obj files:
    - e_ilight.obj: EILight base class (getters/setters, stubs, type info)
    - e_iamblight.obj: EIAmbLight (ambient light)
    - e_idirlight.obj: EIDirLight (directional light with direction)
    - e_ipointlight.obj: EIPointLight (point light with position/falloff)
    - e_ipointamblight.obj: EIPointAmbLight
    - e_ispotlight.obj: EISpotLight
    - e_igameinstance.obj: EIGameInstance
    - e_istaticmodel.obj: EIStaticModel (static model rendering)
    - e_istaticsubmodel.obj: EIStaticSubModel
    - e_mirrorportal.obj: EMirrorPortal (mirror/portal rendering)
    - e_rlevel.obj: ERLevel (render level manager)
    - e_levelman.obj: ELevelManager
    - e_nodelist.obj: ENodeList (linked list)
    - e_mat4.obj: EMat4 (4x4 matrix math)
    - e_quat.obj: EQuat, EVec3Decomp, EEngine, EGraphics extensions
    
    New files:
    - src/render/e_light.cpp + include/render/e_light.h (144 functions)
    - src/render/e_levelman.cpp + include/render/e_levelman.h (37 functions)
    - src/core/e_mat4.cpp + include/core/e_mat4.h (19 functions)

---
+++ include/core/e_mat4.h
@@ -0,0 +1,82 @@
+#ifndef E_MAT4_H
+#define E_MAT4_H
+
+#include "types.h"
+
+// Forward declarations
+class EVec3;
+class EVec4;
+class EQuat;
+
+// EMat4 - 4x4 matrix (64 bytes, row-major: m[row][col])
+// Layout: 4 rows of 4 floats = 16 floats
+// Row 0: m[0x00..0x0C]
+// Row 1: m[0x10..0x1C]
+// Row 2: m[0x20..0x2C]
+// Row 3: m[0x30..0x3C] (translation in last row)
+class EMat4 {
+public:
+    float m[4][4]; // 0x00 - 64 bytes
+
+    void Mult4x4(EMat4& a, EMat4& b);
+    void PreScale(float scale);
+    void PostScale(float scale);
+    void PostTranslate(EVec3& translation);
+
+    void GetColumn(int col, EVec3& out);
+    void GetColumn(int col, EVec4& out);
+    void SetColumn(int col, EVec3& in);
+    void SetColumn(int col, EVec4& in);
+
+    float GetScaleX();
+    float GetScaleY();
+    float GetScaleZ();
+
+    void ExtractAxisRotation(EVec3& axis) const;
+    void Print() const;
+};
+
+// EVec4 - 4D vector (16 bytes)
+struct EVec4 {
+    float x, y, z, w;
+    void Print();
+};
+
+// EQuat - quaternion (16 bytes)
+class EQuat {
+public:
+    float x, y, z, w;
+    void Print();
+};
+
+// EVec3Decomp
+class EBitArray;
+class EVec3Decomp {
+public:
+    u8 pad00[0x18];     // 0x00
+    int m_numComponents; // 0x18
+    s16 m_bitsPerComp;  // 0x1C
+
+    static void Init(EBitArray* arr, int bits);
+    int GetBitCount();
+};
+
+// EEngine
+class ERLevel;
+class EEngine {
+public:
+    u8 pad00[0x30];     // 0x00
+    ERLevel* m_currentLevel; // 0x30
+
+    void ClearCurrentLevel(ERLevel* level);
+    void SetCurrentLevel(ERLevel* level);
+};
+
+// EGraphics extensions from e_quat.obj
+class EGraphics;
+void EGraphics_SetBackgroundColor(EGraphics* g, EVec3& color, int idx, bool flag);
+void EGraphics_GetBackgroundColor(EGraphics* g, EVec3& color, int& outIdx);
+void EGraphics_SetTint(EGraphics* g, EVec3& a, EVec3& b);
+void EGraphics_GetTint(EGraphics* g, EVec3& a, EVec3& b);
+
+#endif // E_MAT4_H
