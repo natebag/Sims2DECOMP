62ae470c71392044bbc46d7369d300edaf3d35e1
natebag <njatobinyt@gmail.com>
2026-04-11 10:21:31 -0400

    match(bulk): +10 matches  matcher-bot shard blast (kmi2)

---
+++ src/matched/agent/match_0x802C949C_EMat4_GetColumn.cpp
@@ -0,0 +1,17 @@
+// FLAGS: -fno-elide-constructors
+// 0x802C949C EMat4::GetColumn (40b)
+
+struct EVec3 {
+    float x, y, z;
+};
+
+struct EMat4 {
+    float m[4][4];
+    void GetColumn(int col, EVec3& out);
+};
+
+void EMat4::GetColumn(int col, EVec3& out) {
+    out.x = m[0][col];
+    out.y = m[1][col];
+    out.z = m[2][col];
+}
