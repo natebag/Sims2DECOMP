92a5868f009451080ccf3bb0c98d95e0816dcfbb
natebag <njatobinyt@gmail.com>
2026-04-11 06:29:58 -0400

    Update ISimInstance type-info getters to use correct pattern

--- src/matched/isiminstance/match_800568AC_ISimInstance__GetTypeInfo.cpp
+++ src/matched/isiminstance/match_800568AC_ISimInstance__GetTypeInfo.cpp
@@ -1,15 +1,4 @@
 // 0x800568AC ISimInstance::GetTypeInfo (12b)
-
-struct TypeInfo {
-    char _pad[8];
-};
-
-extern TypeInfo g_ISimInstance_typeInfo;
-
-struct ISimInstance {
-    virtual TypeInfo* GetTypeInfo() const;
-};
-
-TypeInfo* ISimInstance::GetTypeInfo() const {
-    return &g_ISimInstance_typeInfo;
-}
+struct ETypeInfo { int data[4]; };
+extern ETypeInfo g_ISimInstance_typeInfo;
+ETypeInfo* ISimInstance_GetTypeInfo() { return &g_ISimInstance_typeInfo; }
--- src/matched/isiminstance/match_800568B8_ISimInstance__GetTypeName.cpp
+++ src/matched/isiminstance/match_800568B8_ISimInstance__GetTypeName.cpp
@@ -1,11 +1,4 @@
 // 0x800568B8 ISimInstance::GetTypeName (12b)
-
-extern char g_typeTable[];
-
-struct ISimInstance {
-    virtual const char* GetTypeName() const;
-};
-
-const char* ISimInstance::GetTypeName() const {
-    return *(const char**)(g_typeTable + 12);
-}
+struct ETypeInfo { int data[4]; };
+extern ETypeInfo g_ISimInstance_typeInfo;
+int ISimInstance_GetTypeName() { return g_ISimInstance_typeInfo.data[3]; }
