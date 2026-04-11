664f7ef941a4560d4ae2bb16cd407a07e410c43b
natebag <njatobinyt@gmail.com>
2026-04-11 06:47:57 -0400

    match(timer): SetFrameDelta using -fno-schedule-insns (68B)

---
+++ src/matched/timer/match_80076EF8_Timer_SetFrameDelta.cpp
@@ -0,0 +1,20 @@
+// 0x80076EF8 Timer_SetFrameDelta (68b)
+// FLAGS: -fno-schedule-insns
+
+extern float g_frameDelta;
+extern float g_time;
+extern float g_prevTime;
+extern int g_ticks;
+extern int g_prevTicks;
+extern void Timer_StartFrame();
+
+static const float kFixedDelta = 0.033333335f;
+
+void Timer_SetFrameDelta(float delta) {
+    g_frameDelta = delta;
+    if (delta == kFixedDelta) {
+        g_prevTime = g_time;
+        g_prevTicks = g_ticks;
+    }
+    Timer_StartFrame();
+}
