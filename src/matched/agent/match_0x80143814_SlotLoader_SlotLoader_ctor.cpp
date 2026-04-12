3076817b75d430d1dedc656e2d816a92c7eb4533
natebag <njatobinyt@gmail.com>
2026-04-12 05:10:46 -0400

    Pattern Z/field constructors batch (2 matches)

---
+++ src/matched/agent/match_0x80143814_SlotLoader_SlotLoader_ctor.cpp
@@ -0,0 +1,12 @@
+// 0x80143814 SlotLoader::SlotLoader (20b)
+
+struct SlotLoader {
+    unsigned int field_0;
+    short field_4;
+    SlotLoader(unsigned int p2);
+};
+
+SlotLoader::SlotLoader(unsigned int p2) {
+    field_0 = p2;
+    field_4 = 0;
+}
---
+++ src/matched/e_core/match_0x8023122C_EOrderTableEntry_EOrderTableEntry_ctor.cpp
@@ -0,0 +1,12 @@
+// 0x8023122C EOrderTableEntry::EOrderTableEntry (20b)
+
+struct EOrderTableEntry {
+    int field_0;
+    int field_4;
+    EOrderTableEntry();
+};
+
+EOrderTableEntry::EOrderTableEntry() {
+    field_0 = 0;
+    field_4 = 0;
+}
