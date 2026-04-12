2b31eee834272adaea4881db67db0b0d23a086a0
natebag <njatobinyt@gmail.com>
2026-04-12 06:00:28 -0400

    match(er_level): SetIdMapTableSize

---
+++ src/matched/er_level/match_0x80238268_ERLevel__SetIdMapTableSize.cpp
@@ -0,0 +1,8 @@
+// 0x80238268 ERLevel::SetIdMapTableSize (8B)
+
+struct ERLevel;
+extern void SetIdMapTableSize_impl(ERLevel* self, int size);
+
+void ERLevel__SetIdMapTableSize(void* self, int size) {
+    SetIdMapTableSize_impl((ERLevel*)((char*)self + 0x3A264), size);
+}
