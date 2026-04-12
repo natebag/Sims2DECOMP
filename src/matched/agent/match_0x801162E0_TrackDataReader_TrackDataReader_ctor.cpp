ee79143a46da98dc5deb839cfb8f73aefc0ca5e2
natebag <njatobinyt@gmail.com>
2026-04-12 05:10:01 -0400

    Pattern Z constructors batch 2 (3 matches)

---
+++ src/matched/agent/match_0x801162E0_TrackDataReader_TrackDataReader_ctor.cpp
@@ -0,0 +1,12 @@
+// 0x801162E0 TrackDataReader::TrackDataReader (20b)
+
+struct TrackDataReader {
+    int field_0;
+    int field_4;
+    TrackDataReader();
+};
+
+TrackDataReader::TrackDataReader() {
+    field_0 = 0;
+    field_4 = 0;
+}
---
+++ src/matched/agent/match_0x801B941C_TabInfo_TabInfo_ctor.cpp
@@ -0,0 +1,14 @@
+// 0x801B941C INGTarget::TabInfo::TabInfo (24b)
+
+struct TabInfo {
+    int field_0;
+    short field_4;
+    int field_8;
+    TabInfo();
+};
+
+TabInfo::TabInfo() {
+    field_0 = 0;
+    field_4 = 0;
+    field_8 = 0;
+}
