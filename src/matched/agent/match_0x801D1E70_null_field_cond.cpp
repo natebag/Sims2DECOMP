c1ad06e0883a9a5d8964997133fc784de7e22fd3
natebag <njatobinyt@gmail.com>
2026-04-13 18:11:28 -0400

    fix(dedup): clean 15 dupes

--- src/matched/agent/match_0x801D1E70_null_field_cond.cpp
+++
@@ -1,6 +0,0 @@
-// 0x801D1E70 (48b)
-extern void someFunc(void*, void*);
-void wrapper(char* self) {
-    void* p = *(void**)(self + 4);
-    if (p) someFunc(p, self);
-}
--- src/matched/agent/match_0x801D1EA0_null_field_cond.cpp
+++
@@ -1,6 +0,0 @@
-// 0x801D1EA0 (48b)
-extern void someFunc(void*, void*);
-void wrapper(char* self) {
-    void* p = *(void**)(self + 4);
-    if (p) someFunc(p, self);
-}
