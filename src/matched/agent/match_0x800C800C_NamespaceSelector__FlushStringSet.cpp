3f637ad4d2a5fde1f92688626dd4364fc91a8a62
natebag <njatobinyt@gmail.com>
2026-04-13 15:17:29 -0400

    match(misc): +5 36B-44B DOL cracks

--- src/matched/agent/match_0x800C800C_NamespaceSelector__FlushStringSet.cpp
+++ src/matched/agent/match_0x800C800C_NamespaceSelector__FlushStringSet.cpp
@@ -1,10 +1,16 @@
-// 0x800C800C NamespaceSelector::FlushStringSet(short) (36B)
-void FlushCacheItem(void* self, short key, void* list);
+// 0x800C800C NamespaceSelector::FlushStringSet (36B)
+/* NamespaceSelector::FlushStringSet(short) - 0x800C800C (36 bytes) */
+
+struct DataCacheList { int m_data[2]; };
+
 struct NamespaceSelector {
-    char pad[0x08];
-    char m_strList[1];
-    void FlushStringSet(short key);
+    char m_pad[0x08];
+    DataCacheList m_stringSets;
+
+    void FlushStringSet(short id);
+    void FlushCacheItem(short id, DataCacheList &list);
 };
-void NamespaceSelector::FlushStringSet(short key) {
-    FlushCacheItem(this, key, m_strList);
+
+void NamespaceSelector::FlushStringSet(short id) {
+    FlushCacheItem(id, m_stringSets);
 }
---
+++ src/matched/agent/match_0x800CA978_Neighbor__ResetInterests.cpp
@@ -0,0 +1,17 @@
+// 0x800CA978 Neighbor::ResetInterests (36B)
+/* Neighbor::ResetInterests(void) - 0x800CA978 (36 bytes) */
+
+struct Interests {
+    void SetInterests(void);
+};
+
+struct Neighbor {
+    char m_pad[0x70];
+    Interests m_interests;
+
+    void ResetInterests(void);
+};
+
+void Neighbor::ResetInterests(void) {
+    m_interests.SetInterests();
+}
