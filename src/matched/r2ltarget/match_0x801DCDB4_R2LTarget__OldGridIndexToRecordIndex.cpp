cbe7d636c28d50562098c7ae0a89347f7bc9daf0
natebag <njatobinyt@gmail.com>
2026-04-14 18:33:34 -0400

    Match R2LTarget::OldGridIndexToRecordIndex (0x801DCDB4, 44B)

---
+++ src/matched/r2ltarget/match_0x801DCDB4_R2LTarget__OldGridIndexToRecordIndex.cpp
@@ -0,0 +1,21 @@
+// 0x801DCDB4 R2LTarget::OldGridIndexToRecordIndex (44B)
+
+struct R2LTarget {
+    char pad[0x9C];
+    int m_recordCount;
+    char pad2[0x8];
+    int m_oldGridIndex;
+};
+
+int R2LTarget_OldGridIndexToRecordIndex(R2LTarget *self, int index) {
+    int grid = self->m_oldGridIndex;
+    int offset = grid * 6;
+    int result = index + offset;
+    if (grid < 0) goto fail;
+    if (result < 0) goto fail;
+    if (result < self->m_recordCount) goto success;
+fail:
+    return -1;
+success:
+    return result;
+}
