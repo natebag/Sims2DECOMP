a190ed02db88e15d9a308371874faf9a3568092d
natebag <njatobinyt@gmail.com>
2026-04-14 16:44:04 -0400

    Match

---
+++ src/matched/listbase/match_0x803A36A4_List_base_SoundEventInfo__clear.cpp
@@ -0,0 +1,37 @@
+// 0x803A36A4 _List_base<SoundEventInfo, allocator<SoundEventInfo>>::clear (108B)
+
+struct _List_node_base {
+    _List_node_base *m_next;
+    _List_node_base *m_prev;
+};
+
+template<typename T>
+struct _List_node : _List_node_base {
+    T m_data;
+};
+
+template<typename T>
+struct _List_base {
+    char _pad[4];
+    _List_node_base *m_head;
+    void clear(void);
+};
+
+extern void _List_dealloc(void *p, int size);
+
+template<typename T>
+void _List_base<T>::clear(void) {
+    _List_node_base *cur = m_head->m_next;
+    while (cur != m_head) {
+        _List_node<T> *node = (_List_node<T> *)cur;
+        cur = cur->m_next;
+        if (node)
+            _List_dealloc(node, sizeof(_List_node<T>));
+    }
+    m_head->m_next = m_head;
+    m_head->m_prev = m_head;
+}
+
+struct SoundEventInfo { int a, b, c; };
+
+template void _List_base<SoundEventInfo>::clear(void);
