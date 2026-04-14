0e1d381e83b8b09191fe01d560a2496919f622cb
natebag <njatobinyt@gmail.com>
2026-04-14 18:38:41 -0400

    Match FamilyImpl::GetIndexedMember and GetMemberByGUID (0x800B63A4, 0x800B6334, 40B)

---
+++ src/matched/familyimpl/match_0x800B6334_FamilyImpl__GetMemberByGUID.cpp
@@ -0,0 +1,16 @@
+// 0x800B6334 FamilyImpl::GetMemberByGUID(int) (40B)
+
+struct FamilyImpl {
+    char _pad[0x24];
+    int *m_members;
+    int *m_membersEnd;
+    int *GetMemberByGUID(int guid);
+};
+
+int *FamilyImpl::GetMemberByGUID(int guid) {
+    for (int *p = m_members; p != m_membersEnd; p++) {
+        if (*p == guid)
+            return p;
+    }
+    return 0;
+}
---
+++ src/matched/familyimpl/match_0x800B63A4_FamilyImpl__GetIndexedMember.cpp
@@ -0,0 +1,14 @@
+// 0x800B63A4 FamilyImpl::GetIndexedMember(int) (40B)
+
+struct FamilyImpl {
+    char _pad[0x24];
+    int *m_members;
+    int *m_membersEnd;
+    int *GetIndexedMember(int idx);
+};
+
+int *FamilyImpl::GetIndexedMember(int idx) {
+    if (idx < 0 || idx >= (m_membersEnd - m_members))
+        return 0;
+    return m_members + idx;
+}
