a93db227975e6874b7031bf81e68f8b4bd43146e
natebag <njatobinyt@gmail.com>
2026-04-15 03:08:43 -0400

    SAnimator2: +9 integer matches (SetPlayerControl, UpdateAnimationValidity, checkParticleCleanup, updateMovementState, CheckOptionalMotionAnimStatusOnStateChange, playRumble, triggerCameraBloom, triggerCameraBlur, PreloadDress)

---
+++ src/matched/sanimator2/match_0x8005f7e0_SAnimator2__PreloadDress.cpp
@@ -0,0 +1,18 @@
+/* 0x8005f7e0 (72 bytes) - SAnimator2::PreloadDress(PropRef *) */
+struct PropRef;
+extern char g_strPreloadDress[];
+extern "C" int LookupDress(char* str, int id);
+
+class SAnimator2 {
+public:
+    int getCorrectId(PropRef* prop);
+    int PreloadDress(PropRef* prop);
+};
+
+int SAnimator2::PreloadDress(PropRef* prop) {
+    int id = getCorrectId(prop);
+    if (id == 0) return 0;
+    int result = LookupDress(g_strPreloadDress, id);
+    if (result == 0) return 0;
+    return 1;
+}
