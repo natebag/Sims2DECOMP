b9bae764dba68ccab8475ee03de813d273f9d0e0
natebag <njatobinyt@gmail.com>
2026-04-15 05:18:50 -0400

    Match TextBaseItem::SetVerticalCenter and CreateASimBaseState::CASAccepted

---
+++ src/matched/agent/match_0x80089910_CreateASimBaseState__CASAccepted.cpp
@@ -0,0 +1,18 @@
+// 0x80089910 CreateASimBaseState::CASAccepted(void) (40B)
+
+struct CreateASimBaseState {
+    char pad[0x20];
+    int m_field20;
+    char pad2[4];
+    int m_field28;
+
+    int CASAccepted(void);
+};
+
+int CreateASimBaseState::CASAccepted(void) {
+    int r = 0;
+    if (m_field20 == 0 && m_field28 != 0) {
+        r = 1;
+    }
+    return r;
+}
