71275e398fa3dfefe4fce2b94fd07c48242215fb
natebag <njatobinyt@gmail.com>
2026-04-20 19:47:04 -0400

    Match AptViewer::CallFunction 0x80010358 (60B) - empty varargs body

---
+++ src/matched/aptviewer/match_0x80010358_AptViewer_CallFunction.cpp
@@ -0,0 +1,9 @@
+// 0x80010358 (60B) AptViewer::CallFunction(char*, char*, char*, int, ...)
+
+class AptViewer {
+public:
+    void CallFunction(char* a, char* b, char* c, int d, ...);
+};
+
+void AptViewer::CallFunction(char* a, char* b, char* c, int d, ...) {
+}
