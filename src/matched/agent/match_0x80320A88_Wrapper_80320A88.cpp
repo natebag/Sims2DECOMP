b267924980d840d6ed9f3b6ddba95040ff89ad2c
natebag <njatobinyt@gmail.com>
2026-05-01 15:12:22 -0400

    cleanup: remove legacy Wrapper_<HEX> filename-collision dupes batch 1/7 (kept canonical match_<HEX>_<symbol> versions)

--- src/matched/agent/match_0x80320A88_Wrapper_80320A88.cpp
+++
@@ -1,9 +0,0 @@
-/* operator<<(EStream&, ERTexture*) at 0x80320A88 (32B) */
-// 0x80320A88 (32 bytes)
-class EStream;
-class EStorable {};
-class ERTexture : public EStorable {};
-extern EStream& operator<<(EStream& s, EStorable* obj);
-EStream& operator<<(EStream& s, ERTexture* obj) {
-    return operator<<(s, (EStorable*)obj);
-}
--- src/matched/agent/match_0x80322360_Wrapper_80322360.cpp
+++
@@ -1,9 +0,0 @@
-/* operator<<(EStream&, REffectsAttachment*) at 0x80322360 (32B) */
-// 0x80322360 (32 bytes)
-class EStream;
-class EStorable {};
-class REffectsAttachment : public EStorable {};
-extern EStream& operator<<(EStream& s, EStorable* obj);
-EStream& operator<<(EStream& s, REffectsAttachment* obj) {
-    return operator<<(s, (EStorable*)obj);
-}
