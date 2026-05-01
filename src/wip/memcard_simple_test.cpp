90c495e916d83401f6b584c7b0e9ea7575d69974
natebag <njatobinyt@gmail.com>
2026-05-01 15:20:42 -0400

    cleanup: prune 5 stale src/wip root-level test artifacts older than 2 weeks (no NON_MATCHING / version_diff tags)

--- src/wip/memcard_simple_test.cpp
+++
@@ -1,7 +0,0 @@
-// 0x8007562C SimsMemCardWrap::IsFreeplayUnlocked (16b)
-
-extern char gSimsMemCardWrap[4];
-
-int SimsMemCardWrap_IsFreeplayUnlocked() {
-    return ((int*)gSimsMemCardWrap)[3];  // offset 0x0C / 4
-}
