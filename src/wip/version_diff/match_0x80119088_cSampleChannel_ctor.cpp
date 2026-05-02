e45051ef6fdbc69d29ad668db7d8576c27aef0e1
CodexWorker <codexworker@local>
2026-05-02 03:46:44 -0400

    chore: remove superseded wip stubs (qa-tick #1 cleanup) (NB)

--- src/wip/version_diff/match_0x80119088_cSampleChannel_ctor.cpp
+++
@@ -1,8 +0,0 @@
-// FLAGS: -fno-elide-constructors
-// 0x80119088 cSampleChannel::cSampleChannel (60b)
-void* cSampleChannel_ctor(void* self, void* patch) {
-    *(int*)((char*)self + 0) = 0;
-    *(int*)((char*)self + 4) = 0;
-    ch_init(self);
-    return self;
-}
