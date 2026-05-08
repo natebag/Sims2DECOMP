feb5d0c3e9658138173d179039e4f1c687480da0
natebag <njatobinyt@gmail.com>
2026-05-08 18:44:34 -0400

    match: version_diff promote (+4) — global ctors+dtors+_c2DArray::CopyFrom(BString) post Beta REL14 fix

---
+++ src/matched/agent/match_0x800C2D5C_global_ctor_redAspMotiveDecayMult.cpp
@@ -0,0 +1,7 @@
+/* global constructors keyed to redAspMotiveDecayMult - 44 bytes */
+
+void __static_initialization_and_destruction_0(int init, int priority);
+
+void _GLOBAL__I_redAspMotiveDecayMult(void) {
+    __static_initialization_and_destruction_0(1, 0xFFFF);
+}
