864f263722a671bba018b6736b4cb7b3038ac0d7
natebag <njatobinyt@gmail.com>
2026-05-21 03:24:31 -0400

    chore: clean up 16 duplicate match files surfaced by hook

--- src/matched/agent/match_0x800756BC_SimsMemCardWrap_GetSpaceRequiredForSave.cpp
+++
@@ -1,7 +0,0 @@
-// 0x800756BC SimsMemCardWrap::GetSpaceRequiredForSave (32b)
-
-extern int GetTotalSaveGameSize_void();
-
-int SimsMemCardWrap_GetSpaceRequiredForSave() {
-    return GetTotalSaveGameSize_void();
-}
--- src/matched/ebitarray/match_0x802E00AC_EBitArray__Print.cpp
+++
@@ -1,9 +0,0 @@
-// 0x802E00AC EBitArray::Print(void) (4B)
-
-class EBitArray {
-public:
-    void Print();
-};
-
-void EBitArray::Print() {
-}
