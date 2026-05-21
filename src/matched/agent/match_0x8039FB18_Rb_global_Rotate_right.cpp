da92c595749b0affc92fbeba35fc4226d5cf567b
natebag <njatobinyt@gmail.com>
2026-05-20 23:27:38 -0400

    match: 2x _Rb_global<bool>::_Rotate_left/right (2x 96B) STL RB-tree rotation

---
+++ src/matched/agent/match_0x8039FB18_Rb_global_Rotate_right.cpp
@@ -0,0 +1,32 @@
+// 0x8039FB18 _Rb_global<bool>::_Rotate_right (96B)
+
+struct _Rb_tree_node_base {
+    int _M_color;
+    _Rb_tree_node_base* _M_parent;
+    _Rb_tree_node_base* _M_left;
+    _Rb_tree_node_base* _M_right;
+};
+
+template <typename T> class _Rb_global;
+template <> class _Rb_global<bool> {
+public:
+    static void _Rotate_right(_Rb_tree_node_base* __x, _Rb_tree_node_base*& __root);
+};
+
+void _Rb_global<bool>::_Rotate_right(_Rb_tree_node_base* __x, _Rb_tree_node_base*& __root) {
+    _Rb_tree_node_base* __y = __x->_M_left;
+    __x->_M_left = __y->_M_right;
+    if (__y->_M_right != 0)
+        __y->_M_right->_M_parent = __x;
+    __y->_M_parent = __x->_M_parent;
+
+    if (__x == __root)
+        __root = __y;
+    else if (__x == __x->_M_parent->_M_right)
+        __x->_M_parent->_M_right = __y;
+    else
+        __x->_M_parent->_M_left = __y;
+
+    __y->_M_right = __x;
+    __x->_M_parent = __y;
+}
