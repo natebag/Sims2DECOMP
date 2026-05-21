f5f41c6a76da34bf2d7a45eaa0af32b6639be3f0
natebag <njatobinyt@gmail.com>
2026-05-21 07:12:37 -0400

    match: loadVariables 40B @ 0x80006778 (ASMPROC replace_insn ori->addi)

--- src/matched/agent/match_0x80006778_loadVariables.cpp
+++ src/matched/agent/match_0x80006778_loadVariables.cpp
@@ -1,7 +1,9 @@
-extern char gVarFilePath[];
+// 0x80006778 (40 bytes) loadVariables(char*)
+// ASMPROC_replace_insn: match="ori 3,3,54012" replacement="addi 3,3,54012"
+struct AptValueFactory {
+    static void CreateString(char*);
+};
 
-void LoadVarsImpl(char* path);
-
-void loadVariables(char* name) {
-    LoadVarsImpl(gVarFilePath);
+void loadVariables() {
+    AptValueFactory::CreateString((char*)0x803DD2FC);
 }
