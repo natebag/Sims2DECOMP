0f9845b09e9a01c00471928eeac098f5eb7000af
natebag <njatobinyt@gmail.com>
2026-05-20 23:44:53 -0400

    match: ESpriteRender::SpriteRenderOrderTableCallback 0x80035C08 (72B) volatile-store order

--- src/matched/agent/match_0x80035C08_ESpriteRender__SpriteRenderOrderTableCallback.cpp
+++ src/matched/agent/match_0x80035C08_ESpriteRender__SpriteRenderOrderTableCallback.cpp
@@ -1,31 +1,31 @@
-/* ESpriteRender::SpriteRenderOrderTableCallback(ELevelDrawData &, EOrderTableData *) at 0x80035C08 (72B) */
+// 0x80035C08 ESpriteRender::SpriteRenderOrderTableCallback(ELevelDrawData &, EOrderTableData *) (72B)
 
-struct ERC;
-struct EOrderTableData;
-struct ELevelDrawData;
-
-struct ESpriteRender {
-    char pad_00[0x98];
+class ERC;
+class EOrderTableData;
+class ELevelDrawData {
+public:
+    char _pad[24];
     ERC* m_rc;
-    char pad_9c[0x0C];
-    int m_fieldA8;
-    int m_fieldAC;
-    int m_fieldB0;
-    int m_fieldB4;
-
-    void DrawDirectly(ERC*);
-    void SpriteRenderOrderTableCallback(ELevelDrawData&, EOrderTableData*);
 };
 
-struct ELevelDrawData {
-    char pad[0x18];
-    ESpriteRender* m_sprite;
+class ESpriteRender {
+public:
+    char _pad0[152];
+    ERC* m_field152;
+    char _pad156[168 - 156];
+    int m_field168;
+    int m_field172;
+    int m_field176;
+    int m_field180;
+
+    static void DrawDirectly(ERC* rc, ERC* sprite);
+    void SpriteRenderOrderTableCallback(ELevelDrawData& dd, EOrderTableData* otd);
 };
 
-void ESpriteRender::SpriteRenderOrderTableCallback(ELevelDrawData& data, EOrderTableData*) {
-    data.m_sprite->DrawDirectly(m_rc);
-    m_fieldA8 = 0;
-    m_fieldAC = 0;
-    m_fieldB4 = 0;
-    m_fieldB0 = 0;
+void ESpriteRender::SpriteRenderOrderTableCallback(ELevelDrawData& dd, EOrderTableData* /*otd*/) {
+    DrawDirectly(dd.m_rc, m_field152);
+    *(volatile int*)&m_field176 = 0;
+    *(volatile int*)&m_field168 = 0;
+    *(volatile int*)&m_field172 = 0;
+    *(volatile int*)&m_field180 = 0;
 }
