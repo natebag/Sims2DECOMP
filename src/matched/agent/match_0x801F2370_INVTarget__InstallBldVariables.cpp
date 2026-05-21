ed273a47d9ee794edef47d289d615c5d59befa11
natebag <njatobinyt@gmail.com>
2026-05-21 07:58:37 -0400

    match(INVTarget::InstallBldVariables): 64B hoist fmt address before null check to interleave lis+addi

---
+++ src/matched/agent/match_0x801F2370_INVTarget__InstallBldVariables.cpp
@@ -0,0 +1,26 @@
+// 0x801F2370 INVTarget::InstallBldVariables() (64B)
+
+class CBFunctor;
+
+class GetVariableCommandTable {
+public:
+    void InstallCommand(char* name, CBFunctor& cb);
+};
+
+extern char fmt_InstallBld_803F97C8[];
+
+class INVTarget {
+public:
+    void* m_0;
+    GetVariableCommandTable* m_4;
+    char pad8[32];
+    int m_40_dummy;
+    void InstallBldVariables();
+};
+
+void INVTarget::InstallBldVariables() {
+    char* fmt = fmt_InstallBld_803F97C8;
+    void* p = m_0;
+    if (p == 0) return;
+    m_4->InstallCommand(fmt, *(CBFunctor*)((char*)this + 40));
+}
