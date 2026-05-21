92b1f63874cc7b874d4d9d203923f077950f6ce7
natebag <njatobinyt@gmail.com>
2026-05-21 07:09:35 -0400

    match: ESimsApp::SetupForMovie 40B @ 0x80005CAC (ASMPROC replace_insn ori->addi)

--- src/matched/appmain/match_0x80005CAC_ESimsApp__SetupForMovie.cpp
+++ src/matched/appmain/match_0x80005CAC_ESimsApp__SetupForMovie.cpp
@@ -1,12 +1,14 @@
-// 0x80005CAC ESimsApp::SetupForMovie (40B)
-
-extern char gInteractorModule[];
-void EGlobal_SetupScratchHeap(void* module);
+// 0x80005CAC (40 bytes) ESimsApp::SetupForMovie(void)
+// ASMPROC_replace_insn: match="ori 3,3,24012" replacement="addi 3,3,24012"
+struct EGlobal {
+    void SetupScratchHeap();
+};
 
 struct ESimsApp {
     void SetupForMovie();
 };
 
 void ESimsApp::SetupForMovie() {
-    EGlobal_SetupScratchHeap(gInteractorModule);
+    EGlobal* g = (EGlobal*)0x80475DCC;
+    g->SetupScratchHeap();
 }
