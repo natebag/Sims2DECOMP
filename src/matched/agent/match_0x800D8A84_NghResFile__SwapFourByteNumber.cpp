017046f6f1895d61bee782f469f96b5c396528e8
natebag <njatobinyt@gmail.com>
2026-05-22 07:36:09 -0400

    match(agent): NghResFile::SwapFourByteNumber @ 0x800D8A84 (36B)

--- src/matched/agent/match_0x800D8A84_NghResFile__SwapFourByteNumber.cpp
+++ src/matched/agent/match_0x800D8A84_NghResFile__SwapFourByteNumber.cpp
@@ -1,17 +1,18 @@
-// 0x800D8A84 NghResFile::SwapFourByteNumber(unsigned int*) (36B)
+// FLAGS: -fno-schedule-insns
+// 0x800D8A84 NghResFile::SwapFourByteNumber(unsigned int *) (36B)
 
 struct NghResFile {
-    void SwapFourByteNumber(unsigned int* p);
+    void SwapFourByteNumber(unsigned int* ptr);
 };
 
-void NghResFile::SwapFourByteNumber(unsigned int* p) {
-    unsigned char* b = (unsigned char*)p;
-    unsigned char b3 = *(volatile unsigned char*)&b[3];
-    unsigned char b0 = *(volatile unsigned char*)&b[0];
-    unsigned char b2 = *(volatile unsigned char*)&b[2];
-    unsigned char b1 = *(volatile unsigned char*)&b[1];
-    *(volatile unsigned char*)&b[1] = b2;
-    *(volatile unsigned char*)&b[3] = b0;
-    *(volatile unsigned char*)&b[0] = b3;
-    *(volatile unsigned char*)&b[2] = b1;
+void NghResFile::SwapFourByteNumber(unsigned int* ptr) {
+    unsigned char* p = (unsigned char*)ptr;
+    unsigned char b3 = p[3];
+    unsigned char b0 = p[0];
+    unsigned char b2 = p[2];
+    unsigned char b1 = p[1];
+    p[1] = b2;
+    p[3] = b0;
+    p[0] = b3;
+    p[2] = b1;
 }
