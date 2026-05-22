a8de358d9e47340258d3b1f0378bbb66109b6b0b
natebag <njatobinyt@gmail.com>
2026-05-22 11:13:04 -0400

    match: 0x80037A5C EyeToyClient::ReleaseTexture 64B

--- src/matched/agent/match_0x80037A5C_EyeToyClient__ReleaseTexture.cpp
+++ src/matched/agent/match_0x80037A5C_EyeToyClient__ReleaseTexture.cpp
@@ -1,22 +1,43 @@
 // 0x80037A5C EyeToyClient::ReleaseTexture(ETexture*) (64B)
-struct ETexture;
 
-struct VTable {
-    char pad[192];
-    short m_adj;
-    short m_pad;
-    void (*m_fn)(void*, ETexture*);
+class ETexture;
+
+class EManager {
+public:
+    virtual ~EManager() = 0;
+    virtual void f2() = 0;
+    virtual void f3() = 0;
+    virtual void f4() = 0;
+    virtual void f5() = 0;
+    virtual void f6() = 0;
+    virtual void f7() = 0;
+    virtual void f8() = 0;
+    virtual void f9() = 0;
+    virtual void f10() = 0;
+    virtual void f11() = 0;
+    virtual void f12() = 0;
+    virtual void f13() = 0;
+    virtual void f14() = 0;
+    virtual void f15() = 0;
+    virtual void f16() = 0;
+    virtual void f17() = 0;
+    virtual void f18() = 0;
+    virtual void f19() = 0;
+    virtual void f20() = 0;
+    virtual void f21() = 0;
+    virtual void f22() = 0;
+    virtual void f23() = 0;
+    virtual void ReleaseTexture(ETexture* t) = 0;
 };
 
-struct Engine {
-    VTable* m_vt;
+extern EManager* g_eyetoy_mgr;
+
+class EyeToyClient {
+public:
+    static void ReleaseTexture(ETexture* tex);
 };
 
-extern Engine* g_engine;  // SDA (at r13-26392)
-
-void EyeToyClient__ReleaseTexture(ETexture* tex) {
+void EyeToyClient::ReleaseTexture(ETexture* tex) {
     if (tex == 0) return;
-    Engine* e = g_engine;
-    VTable* vt = e->m_vt;
-    vt->m_fn((char*)e + vt->m_adj, tex);
+    g_eyetoy_mgr->ReleaseTexture(tex);
 }
