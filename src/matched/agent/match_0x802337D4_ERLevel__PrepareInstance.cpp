efe4dace9994107466611764982a38dab67e7757
natebag <njatobinyt@gmail.com>
2026-05-22 11:03:32 -0400

    match: 0x802337D4 ERLevel::PrepareInstance 80B

---
+++ src/matched/agent/match_0x802337D4_ERLevel__PrepareInstance.cpp
@@ -0,0 +1,58 @@
+// 0x802337D4 ERLevel::PrepareInstance(EInstance*) (80B)
+// MI vcall at slot 38 (offset 0x130/0x134)
+
+class ERLevel;
+
+class EInstance {
+public:
+    virtual ~EInstance() = 0;
+    virtual void f2() = 0;
+    virtual void f3() = 0;
+    virtual void f4() = 0;
+    virtual void f5() = 0;
+    virtual void f6() = 0;
+    virtual void f7() = 0;
+    virtual void f8() = 0;
+    virtual void f9() = 0;
+    virtual void f10() = 0;
+    virtual void f11() = 0;
+    virtual void f12() = 0;
+    virtual void f13() = 0;
+    virtual void f14() = 0;
+    virtual void f15() = 0;
+    virtual void f16() = 0;
+    virtual void f17() = 0;
+    virtual void f18() = 0;
+    virtual void f19() = 0;
+    virtual void f20() = 0;
+    virtual void f21() = 0;
+    virtual void f22() = 0;
+    virtual void f23() = 0;
+    virtual void f24() = 0;
+    virtual void f25() = 0;
+    virtual void f26() = 0;
+    virtual void f27() = 0;
+    virtual void f28() = 0;
+    virtual void f29() = 0;
+    virtual void f30() = 0;
+    virtual void f31() = 0;
+    virtual void f32() = 0;
+    virtual void f33() = 0;
+    virtual void f34() = 0;
+    virtual void f35() = 0;
+    virtual void f36() = 0;
+    virtual void f37() = 0;
+    virtual void DoPrep(ERLevel* level) = 0;
+};
+
+class ERLevel {
+public:
+    void AddInstanceToIdMap(EInstance* inst);
+    void PrepareInstance(EInstance* inst);
+};
+
+void ERLevel::PrepareInstance(EInstance* inst) {
+    AddInstanceToIdMap(inst);
+    *(ERLevel**)((char*)inst + 4) = this;
+    inst->DoPrep(this);
+}
