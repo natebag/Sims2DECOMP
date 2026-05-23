f17b39bea9c5ce7cbb5c56eebb55da3178840922
natebag <njatobinyt@gmail.com>
2026-05-23 01:42:46 -0400

    cleanup: remove dupe GetSAnimator stubs; cxpersonimpl/ is canonical

--- src/matched/agent/match_0x8012C7EC_cXPersonImpl__GetSAnimator_void.cpp
+++
@@ -1,4 +0,0 @@
-// 0x8012C7EC cXPersonImpl::GetSAnimator(void) (8 B)
-// FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lwz 3,0x3f4(3)"
-extern "C" int f_8012C7EC() {}
--- src/wip/duplicates/match_8012C7EC_cXPersonImpl__GetSAnimator.cpp
+++
@@ -1,9 +0,0 @@
-// 0x8012C7EC (8 bytes)
-class cXPersonImpl {
-public:
-    int GetSAnimator();
-};
-
-int cXPersonImpl::GetSAnimator() {
-    return *(int*)((char*)this + 0x3F4);
-}
