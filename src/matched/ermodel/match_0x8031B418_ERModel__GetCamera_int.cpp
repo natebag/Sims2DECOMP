0b174df8c217989b132beced11971049b4406624
natebag <njatobinyt@gmail.com>
2026-05-31 00:56:00 -0400

    match: ERModel::GetCamera 0x8031B418 (16B) clean slot-index stride 88; drop forced ASMPROC inject

--- src/matched/ermodel/match_0x8031B418_ERModel__GetCamera_int.cpp
+++ src/matched/ermodel/match_0x8031B418_ERModel__GetCamera_int.cpp
@@ -1,10 +1,14 @@
-// 0x8031B418 ERModel::GetCamera(int) (16 B)
 // FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="mulli 4,4,88; lwz 3,0x128(3); add 3,3,4"
+// 0x8031B418 ERModel::GetCamera(int) (16B)
+// mulli r4,r4,88; lwz r3,0x128(3); add r3,r3,r4; blr  -- &m_cameras[i], stride 88
+struct ERCamera { char data[88]; };
 
 struct ERModel {
-    void GetCamera();
+    char pad[0x128];
+    ERCamera* m_cameras;    // 0x128
+    ERCamera* GetCamera(int i);
 };
 
-void ERModel::GetCamera() {
+ERCamera* ERModel::GetCamera(int i) {
+    return &m_cameras[i];
 }
