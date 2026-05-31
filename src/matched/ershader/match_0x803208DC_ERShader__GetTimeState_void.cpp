f9aeaac2990f9f4ce3b437b1ef5d591482d35936
natebag <njatobinyt@gmail.com>
2026-05-31 02:52:42 -0400

    match: ERShader::GetTimeState 0x803208DC (40B) clean magic-div getter (unsigned char)(m_66*4 + m_67/15); default scheduling reproduces DOL load interleave; ASMPROC stub redone clean

--- src/matched/ershader/match_0x803208DC_ERShader__GetTimeState_void.cpp
+++ src/matched/ershader/match_0x803208DC_ERShader__GetTimeState_void.cpp
@@ -1,10 +1,12 @@
-// 0x803208DC ERShader::GetTimeState(void) (40 B)
-// FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lbz 9,0x67(3); lis 0,-30584; ori 0,0,34953; lbz 3,0x66(3); mulhwu 9,9,0; rlwinm 3,3,2,0,29; rlwinm 9,9,29,3,31; add 3,3,9; rlwinm 3,3,0,24,31"
+// 0x803208DC ERShader::GetTimeState(void) (40B)
 
 struct ERShader {
-    void GetTimeState();
+    char _pad0[0x66];
+    unsigned char m_66;   // 0x66 = 102
+    unsigned char m_67;   // 0x67 = 103
+    unsigned char GetTimeState();
 };
 
-void ERShader::GetTimeState() {
+unsigned char ERShader::GetTimeState() {
+    return (unsigned char)(m_66 * 4 + m_67 / 15);
 }
