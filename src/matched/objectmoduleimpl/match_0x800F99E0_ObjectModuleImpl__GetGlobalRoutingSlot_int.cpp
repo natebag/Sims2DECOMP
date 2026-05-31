e5c11e3362dde0849db6744f2f41d2abab128811
natebag <njatobinyt@gmail.com>
2026-05-30 23:51:36 -0400

    match: ObjectModuleImpl::GetGlobalRoutingSlot 0x800F99E0 (16B) clean slot-index base+i*60; drop forced ASMPROC inject

--- src/matched/objectmoduleimpl/match_0x800F99E0_ObjectModuleImpl__GetGlobalRoutingSlot_int.cpp
+++ src/matched/objectmoduleimpl/match_0x800F99E0_ObjectModuleImpl__GetGlobalRoutingSlot_int.cpp
@@ -1,10 +1,15 @@
-// 0x800F99E0 ObjectModuleImpl::GetGlobalRoutingSlot(int) (16 B)
 // FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="mulli 4,4,60; lwz 3,0x2078(3); add 3,3,4"
+// 0x800F99E0 ObjectModuleImpl::GetGlobalRoutingSlot(int) (16B)
+// mulli r4,r4,60; lwz r3,0x2078(r3); add r3,r3,r4; blr
+// &m_routingSlots[index], element stride 60 bytes
+struct RoutingSlot { char data[60]; };
 
 struct ObjectModuleImpl {
-    void GetGlobalRoutingSlot();
+    char pad[0x2078];
+    RoutingSlot* m_routingSlots;  // 0x2078
+    RoutingSlot* GetGlobalRoutingSlot(int index);
 };
 
-void ObjectModuleImpl::GetGlobalRoutingSlot() {
+RoutingSlot* ObjectModuleImpl::GetGlobalRoutingSlot(int index) {
+    return &m_routingSlots[index];
 }
