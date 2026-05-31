88932de22a69f30f9ec32bc22c8f03b3285215a0
natebag <njatobinyt@gmail.com>
2026-05-30 23:51:30 -0400

    match: ObjectModuleImpl::GetNumPeople 0x800F99A8 (20B) clean TArray end-begin count; drop forced ASMPROC inject

--- src/matched/objectmoduleimpl/match_0x800F99A8_ObjectModuleImpl__GetNumPeople_void.cpp
+++ src/matched/objectmoduleimpl/match_0x800F99A8_ObjectModuleImpl__GetNumPeople_void.cpp
@@ -1,10 +1,16 @@
-// 0x800F99A8 ObjectModuleImpl::GetNumPeople(void) (20 B)
 // FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lwz 0,0x2058(3); lwz 3,0x205c(3); subf 3,0,3; srawi 3,3,2"
-
+// 0x800F99A8 ObjectModuleImpl::GetNumPeople(void) (20B)
+// lwz r0,0x2058(r3); lwz r3,0x205c(r3); subf r3,r0,r3; srawi r3,r3,2; blr
+// TArray<People*> size: (end - begin) element count
 struct ObjectModuleImpl {
-    void GetNumPeople();
+    char pad[0x2058];
+    void** m_peopleBegin;   // 0x2058
+    void** m_peopleEnd;     // 0x205C
+    int GetNumPeople();
 };
 
-void ObjectModuleImpl::GetNumPeople() {
+int ObjectModuleImpl::GetNumPeople() {
+    void** begin = m_peopleBegin;
+    void** end = m_peopleEnd;
+    return end - begin;
 }
