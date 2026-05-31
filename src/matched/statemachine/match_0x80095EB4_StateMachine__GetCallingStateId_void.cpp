841322d75a2d38888cd28bc2630d4c40702d852e
natebag <njatobinyt@gmail.com>
2026-05-31 00:14:04 -0400

    match: StateMachine::GetCallingStateId 0x80095EB4 (12B) clean two-deref getter; drop forced ASMPROC inject

--- src/matched/statemachine/match_0x80095EB4_StateMachine__GetCallingStateId_void.cpp
+++ src/matched/statemachine/match_0x80095EB4_StateMachine__GetCallingStateId_void.cpp
@@ -1,10 +1,14 @@
-// 0x80095EB4 StateMachine::GetCallingStateId(void) (12 B)
 // FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lwz 9,0x48(3); lwz 3,0x0(9)"
-
-struct StateMachine {
-    void GetCallingStateId();
+// 0x80095EB4 StateMachine::GetCallingStateId(void) (12B)
+// lwz r9,0x48(3); lwz r3,0x0(9); blr  -- this->m_callingState->m_id
+struct SMState {
+    int m_id;           // 0x0
 };
 
-void StateMachine::GetCallingStateId() {
-}
+struct StateMachine {
+    char pad[0x48];
+    SMState* m_callingState;    // 0x48
+    int GetCallingStateId();
+};
+
+int StateMachine::GetCallingStateId() { return m_callingState->m_id; }
