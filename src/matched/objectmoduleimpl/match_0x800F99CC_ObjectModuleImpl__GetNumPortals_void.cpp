0625cb3afe7bee6b1c41cb77e333a168f0fe93a2
natebag <njatobinyt@gmail.com>
2026-05-30 23:51:34 -0400

    match: ObjectModuleImpl::GetNumPortals 0x800F99CC (20B) clean TArray end-begin count; drop forced ASMPROC inject

--- src/matched/objectmoduleimpl/match_0x800F99CC_ObjectModuleImpl__GetNumPortals_void.cpp
+++ src/matched/objectmoduleimpl/match_0x800F99CC_ObjectModuleImpl__GetNumPortals_void.cpp
@@ -1,10 +1,16 @@
-// 0x800F99CC ObjectModuleImpl::GetNumPortals(void) (20 B)
 // FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lwz 0,0x2068(3); lwz 3,0x206c(3); subf 3,0,3; srawi 3,3,2"
-
+// 0x800F99CC ObjectModuleImpl::GetNumPortals(void) (20B)
+// lwz r0,0x2068(r3); lwz r3,0x206c(r3); subf r3,r0,r3; srawi r3,r3,2; blr
+// TArray<Portal*> size: (end - begin) element count
 struct ObjectModuleImpl {
-    void GetNumPortals();
+    char pad[0x2068];
+    void** m_portalsBegin;  // 0x2068
+    void** m_portalsEnd;    // 0x206C
+    int GetNumPortals();
 };
 
-void ObjectModuleImpl::GetNumPortals() {
+int ObjectModuleImpl::GetNumPortals() {
+    void** begin = m_portalsBegin;
+    void** end = m_portalsEnd;
+    return end - begin;
 }
