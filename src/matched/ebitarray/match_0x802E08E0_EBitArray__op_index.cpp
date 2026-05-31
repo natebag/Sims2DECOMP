9652f36c8dfe72932e57ae1edda9a763ad19227c
natebag <njatobinyt@gmail.com>
2026-05-31 00:28:35 -0400

    match: EBitArray::operator[] 0x802E08E0 (16B) clean sret BitRef(this,index) ctor; drop forced ASMPROC insert_mr

--- src/matched/ebitarray/match_0x802E08E0_EBitArray__op_index.cpp
+++ src/matched/ebitarray/match_0x802E08E0_EBitArray__op_index.cpp
@@ -1,9 +1,17 @@
-// FLAGS: -fno-schedule-insns
-// ASMPROC_insert_mr: before="stw 4,0(3)" src=3 dst=9 relabel=3:9 until=blr
-
-struct EBitArray {
-    void op_index();
+// FLAGS: -fno-schedule-insns2
+// 0x802E08E0 EBitArray::operator[](int) (16B)
+// sret: mr r9,r3; stw r4,0(r9); stw r5,4(r9); blr  -- returns BitRef(this, index)
+struct EBitArray;
+struct EBitArrayBitRef {
+    EBitArray* m_owner;     // 0x0
+    int m_index;            // 0x4
+    EBitArrayBitRef(EBitArray* owner, int index) : m_owner(owner), m_index(index) {}
 };
 
-void EBitArray::op_index() {
+struct EBitArray {
+    EBitArrayBitRef operator[](int index);
+};
+
+EBitArrayBitRef EBitArray::operator[](int index) {
+    return EBitArrayBitRef(this, index);
 }
