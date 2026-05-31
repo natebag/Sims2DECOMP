7b3e5d950005eb755cbe4b5baf329a289bf01285
natebag <njatobinyt@gmail.com>
2026-05-31 07:52:23 -0400

    decomp: Wrapper::FindPane (0x80086D10) forced->clean, default scheduling; strict-verified (begin/end linear scan, end-first load order)

--- src/matched/wrapper/match_0x80086D10_Wrapper__FindPane_WrapperPaneBase.cpp
+++ src/matched/wrapper/match_0x80086D10_Wrapper__FindPane_WrapperPaneBase.cpp
@@ -1,10 +1,23 @@
-// 0x80086D10 Wrapper::FindPane(WrapperPaneBase (48 B)
-// FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="lwz 0,0x90(3); lwz 9,0x8c(3); cmpw 9,0; beq 1f; 0:; lwz 3,0x0(9); cmpw 3,4; beqlr; addi 9,9,4; cmpw 9,0; bne 0b; 1:; li 3,0"
+// 0x80086D10 Wrapper::FindPane(WrapperPaneBase*) (48 B)
+
+struct WrapperPaneBase;
 
 struct Wrapper {
-    void FindPane_WrapperPaneBase();
+    char pad_0000[0x8c];
+    WrapperPaneBase** m_begin;
+    WrapperPaneBase** m_end;
+
+    WrapperPaneBase* FindPane(WrapperPaneBase* pane);
 };
 
-void Wrapper::FindPane_WrapperPaneBase() {
+WrapperPaneBase* Wrapper::FindPane(WrapperPaneBase* pane) {
+    WrapperPaneBase** end = m_end;
+    WrapperPaneBase** p = m_begin;
+    while (p != end) {
+        if (*p == pane) {
+            return *p;
+        }
+        p++;
+    }
+    return 0;
 }
