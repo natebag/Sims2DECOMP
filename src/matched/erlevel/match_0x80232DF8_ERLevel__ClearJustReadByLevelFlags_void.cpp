8338724311433bc7e7a38906850ec4812bcbcdc1
natebag <njatobinyt@gmail.com>
2026-06-05 06:17:57 -0400

    decomp: ERLevel ClearJustReadByLevelFlags (0x80232DF8) clean C++ match

--- src/matched/erlevel/match_0x80232DF8_ERLevel__ClearJustReadByLevelFlags_void.cpp
+++ src/matched/erlevel/match_0x80232DF8_ERLevel__ClearJustReadByLevelFlags_void.cpp
@@ -1,10 +1,16 @@
 // 0x80232DF8 ERLevel::ClearJustReadByLevelFlags(void) (68 B)
-// FLAGS: -fno-schedule-insns
-// ASMPROC_inject_before: before="blr" lines="addis 3,3,3; li 10,0; lwz 0,-15688(3); cmpw 10,0; bgelr; li 8,0; 0:; addi 9,3,-23880; addi 10,10,1; lwzx 11,9,8; addi 8,8,4; lwz 0,0x5c(11); rlwinm 0,0,0,18,16; stw 0,0x5c(11); lwz 9,-15688(3); cmpw 10,9; blt 0b"
 
+struct EInstance { unsigned flags[24]; };
 struct ERLevel {
+    char pad0[0x2A2B8];
+    EInstance* instances[0x800];
+    char pad1[0x2000 - 0x800 * 4];
+    int instanceCount;
     void ClearJustReadByLevelFlags();
 };
 
 void ERLevel::ClearJustReadByLevelFlags() {
+    for (int i = 0; i < instanceCount; i++) {
+        instances[i]->flags[23] &= ~0x4000;
+    }
 }
